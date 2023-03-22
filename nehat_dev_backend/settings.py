4baf5c9587ae24bddd044a73881130559657fa7d
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2023-03-23 00:52:44 +0530

    .

--- nehat_dev_backend/settings.py
+++ nehat_dev_backend/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+try:
+    from .local import *
+except:
+    pass
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,7 +143,4 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-try:
-    from .local import *
-except:
-    pass
+
