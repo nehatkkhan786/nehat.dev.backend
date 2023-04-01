5214922a73f740482d11f30d6d143727bb03b7b6
nehatkkhan786 <33215298+nehatkkhan786@users.noreply.github.com>
2023-04-01 12:25:43 +0530

    v

--- nehat_dev_backend/settings.py
+++ nehat_dev_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z%uif8f0#pcrx#^@3ks1nf7v_j^5y=wwo4qc)@dnhor9et5z$9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # CORS_ALLOWED_ORIGINS = [
 #     "https://api.nehat.dev",
 #     "https://www.api.nehat.dev",
